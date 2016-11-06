acf36cf885a692612d7541559fc9473e3087f0af
Rachel Beneroff <rbbeneroff@gmail.com>
2016-11-06 17:42:36 -0500

    cleaned up code

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(favicon(__dirname + '/favicon.ico'));
 
 app.use('/scripts', express.static(__dirname + '/bower_components'));
 
-
 app.get('/', function(req, res) {
   res.render('index');
 });
@@ -18,7 +17,6 @@ app.get('/', function(req, res) {
 app.use(function(req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
-
 
 app.listen(process.env.PORT || 3000, function() {
 console.log('I HERE ---> 3000');
